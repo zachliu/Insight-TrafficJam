428453df42b5234ee87f59dcd039299371c184ec
zachliu <zach.z.liu@gmail.com>
2015-10-02 04:10:43 +0000

    solved query slow problem

--- flask/app/static/map.js
+++ flask/app/static/map.js
@@ -3,6 +3,7 @@ var NY = new google.maps.LatLng(43.036821, -75.934096);
 var map;
 var roads = [];
 var all_roads = [];
+var REFRESH = 0;
 
 var Colors = [
     "#20CA20",    // green
@@ -17,65 +18,18 @@ function initialize() {
 	center: NY
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    google.maps.event.addListener(map, "rightclick", function(event) {
-        var lat = event.latLng.lat();
-        var lng = event.latLng.lng();
-        // populate yor box/field with lat, lng
-        alert("Lat=" + lat + "; Lng=" + lng);
-    });
+/*
     $.getJSON('/all_roads',
         function(data) {
-            rds = data.roads
-            for (var i = 0; i < rds.length; i = i + 1) {
-                var highlight = [];
-                var stid = rds[i].name;
-                for (var j = 0; j < rds[i].roadloc.length; j = j + 1) {
-                    highlight.push(new google.maps.LatLng(rds[i].roadloc[j][1], rds[i].roadloc[j][0]))
-                }
-                var route = new google.maps.Polyline({
-                    path: highlight,
-                    strokeColor: "#989899",
-                    strokeOpacity: 0.7,
-                    strokeWeight: 2,
-                    title: stid,
-                    map: map
-                });
-                google.maps.event.addListener(route, "click", function(event) {
-                    //var lat = event.latLng.lat();
-                    //var lng = event.latLng.lng();
-                    // populate yor box/field with lat, lng
-                    //alert("Lat=" + lat + "; Lng=" + lng);
-                    //alert(this.title)
-                    var base_url = "http://54.174.177.48/batch/";
-                    var key = this.title;
-                    window.open(base_url.concat(key));
-                });
-                //all_roads.push(route);
-            }
+            net_layer = new google.maps.Data();
+            net_layer.addGeoJson(data);
+            net_layer.setMap(map);
+            //map.data.addGeoJson(data);
         }
     );
-}
-
-/*
-function chooseColor(cc) {
-    if (cc > 2000) {
-        return Colors[3];
-    }
-    else if (cc > 1000) {
-        return Colors[2];
-    }
-    else if (cc > 500) {
-        return Colors[1];
-    }
-    else {
-        return Colors[0];
-    }
-}
 */
 
 /*
-function update_values_road_test() {
     $.getJSON('/realtime_roads',
         function(data) {
             rds = data.roads
@@ -96,12 +50,7 @@ function update_values_road_test() {
                     map: map
                 });
                 google.maps.event.addListener(route, "click", function(event) {
-                    //var lat = event.latLng.lat();
-                    //var lng = event.latLng.lng();
-                    // populate yor box/field with lat, lng
-                    //alert("Lat=" + lat + "; Lng=" + lng);
-                    //alert(this.title)
-                    var base_url = "http://54.174.177.48/batch/";
+                    var base_url = "http://52.23.167.189/batch/";
                     var key = this.title;
                     window.open(base_url.concat(key));
                 });
@@ -109,20 +58,85 @@ function update_values_road_test() {
             }
         }
     );
-    window.setTimeout(update_values_road_test, 2500);
-}
 */
 
-//update_values();
-//update_values_road_test();
+}
 
-/*
+function toggleNetwork() {
+    if (REFRESH == 1) {
+        alert("Stop refreshing");
+        REFRESH = 0;
+        update_values();
+    }
+    else {
+        alert("Start refreshing");
+        REFRESH = 1;
+        update_values();
+    }
+}
+
+function chooseColor(cc) {
+    if (cc > 2000) {
+        return Colors[3];
+    }
+    else if (cc > 1000) {
+        return Colors[2];
+    }
+    else if (cc > 500) {
+        return Colors[1];
+    }
+    else {
+        return Colors[0];
+    }
+}
+
+
+function update_values() {
+    $.getJSON('/realtime_roads',
+        function(data) {
+            rds = data.roads
+            clearRoads();
+            for (var i = 0; i < rds.length; i = i + 1) {
+                var highlight = [];
+                var cc = rds[i].carcount;
+                var stid = rds[i].name;
+                for (var j = 0; j < rds[i].roadloc.length; j = j + 1) {
+                    highlight.push(new google.maps.LatLng(rds[i].roadloc[j][1], rds[i].roadloc[j][0]))
+                }
+                var route = new google.maps.Polyline({
+                    path: highlight,
+                    strokeColor: chooseColor(Number(cc)),
+                    strokeOpacity: 1,
+                    strokeWeight: 5,
+                    title: stid,
+                    map: map
+                });
+                google.maps.event.addListener(route, "click", function(event) {
+                    var base_url = "http://52.23.167.189/batch/";
+                    var key = this.title;
+                    window.open(base_url.concat(key));
+                });
+                roads.push(route);
+            }
+        }
+    );
+    if (REFRESH == 1) {
+        window.setTimeout(update_values, 1500);
+    }
+    else {
+        window.setTimeout(update_values, 3600000);
+    }
+}
+
+
+update_values();
+
+
 function clearRoads() {
     for (var i = 0; i < roads.length; i++) {
 	roads[i].setMap(null);
     }
     roads = [];
 }
-*/
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- flask/app/views.py
+++ flask/app/views.py
@@ -2,27 +2,19 @@ from app import app
 from cassandra.cluster import Cluster
 import flask
 from flask import jsonify, render_template
-#from flask import Flask
-#from flask import jsonify, render_template, request
 import json
+import geojson
+from geojson import Feature, MultiLineString, FeatureCollection
+import time
 
 #app = flask.Flask(__name__)
-
-#@app.route("/")
-#@app.route("/real/")
-#def hello():
-#    return "StID, Car Count"
 
 #cluster = Cluster(['54.175.15.242'])
 cluster = Cluster(['54.174.177.48'])
 session_real = cluster.connect()
 session_batch = cluster.connect()
-session_rdd = cluster.connect()
-session = cluster.connect()
-session_all = cluster.connect()
-findname = cluster.connect()
-findcoor = cluster.connect()
-findroad = cluster.connect()
+session_findname = cluster.connect()
+session_findloc = cluster.connect()
 
 
 @app.route('/')
@@ -45,45 +37,78 @@ def aboutme():
 def realtime_roads():
     session_real.set_keyspace("keyspace_realtime")
     rows = session_real.execute("SELECT * FROM mytable")
-    findroad.set_keyspace("road_geoloc")
+    session_findloc.set_keyspace("road_geoloc")
     roads = []
+    total = 0
+
+    start = time.time()
+    lookup = {}
+    cass = session_findloc.execute("SELECT * FROM header")
+    #print cass
+
+    for e in cass:
+        lookup[str(e.stid)] = str(e.coord)
+    print time.time() - start
+    #print lookup
 
     for row in rows:
-        stid = row[0].split('\'')[0]
+        stid = str(row[0].split('\'')[0])
         cc = row[2].split('\'')[0]
-        locstring = findroad.execute("SELECT * FROM header WHERE stid = '%s'" % stid)
+        start = time.time()
+        locstring = ''
+        if stid in lookup:
+            locstring = lookup[stid]
+        #locstring = session_findloc.execute("SELECT * FROM header WHERE stid = '%s'" % stid)
+        total += time.time() - start
+
         if len(locstring) is not 0:
             roadloc = []
-            listofpairs = locstring[0].coord.split(';')
+            #listofpairs = locstring[0].coord.split(';')
+            listofpairs = locstring.split(';')
             for entry in listofpairs:
                 roadloc.append(entry.split(','))
             roads.append({'name': stid, 'carcount': cc, 'roadloc': roadloc})
+    print "refresh realtime roads info " + str(len(roads)) + " " + str(total)
     return jsonify(roads=roads)
 
 
+session_all = cluster.connect()
 @app.route('/all_roads')
 def all_roads():
     session_all.set_keyspace("road_geoloc")
     rows = session_all.execute("SELECT * FROM header")
-    roads = []
+    #roads = []
+    array = []
     for row in rows:
-        stid = row[0].split('\'')[0]
+        #stid = row[0].split('\'')[0]
         locstring = row[1].split('\'')[0]
         if len(locstring) is not 0:
             roadloc = []
             listofpairs = locstring.split(';')
             for entry in listofpairs:
-                roadloc.append(entry.split(','))
-            roads.append({'name': stid, 'roadloc': roadloc})
-    return jsonify(roads=roads)
+                lat, lon = entry.split(',')
+                roadloc.append((float(lat), float(lon)))
+            #roads.append({'name': stid, 'roadloc': roadloc})
+            array.append(roadloc)
+    #print roadloc
+    formated = MultiLineString(array)
+    my_feature = Feature(geometry=formated)
+    coll = FeatureCollection([my_feature])
+    print "hello i am called"
+    return jsonify(coll)
+    #return jsonify(formated=formated)
 
 
 @app.route("/batch/<stid>")
 def hichart(stid):
     #print stid
-    stmt = "SELECT yyyymm, carcount FROM keyspace_batch.mytable_RDD WHERE roadid = %s"
-    response = session.execute(stmt, parameters=[int(stid)])
+    query = "SELECT yyyymm, carcount FROM keyspace_batch.mytable_RDD WHERE roadid = %s"
+    response = session_batch.execute(query, parameters=[int(stid)])
 
+    query = "SELECT rdn FROM station_header.header WHERE stid = %s"
+    name = session_findname.execute(query, parameters=[stid])
+    stname = str(name[0].rdn.split('\'')[0])
+    print type(stname)
     tmp_dict = {}
     for val in response:
         print val
@@ -91,13 +116,14 @@ def hichart(stid):
 
     #tmp_dict_descending = OrderedDict(sorted(tmp_dict.items(), key=lambda kv: kv[1], reverse=True))
 
-    jsonresponse = []
+    array = []
     for key in tmp_dict:
-        jsonresponse.append({"yyyymm": key, "carcount": tmp_dict[key]})
+        array.append(tmp_dict[key])
+        #jsonresponse.append({"yyyymm": key, "carcount": tmp_dict[key]})
 
-    jsonresponse = list(reversed(jsonresponse))
-    return render_template("batch_test.html", jsonresponse=jsonresponse)
-
+    #jsonresponse = list(reversed(jsonresponse))
+    return render_template("batch_query.html", array=array, stname=stname)
+    #return jsonify(jsonresponse=jsonresponse)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
